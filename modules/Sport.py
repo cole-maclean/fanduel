2a22484f92c1ef77094d2fba07dd418c0420e295
Cole MacLean <maclean.cole@gmail.com>
2015-04-20 16:39:26 -0600

    built optimum_roster method. MLB sport class to the point where we can obtain an optimal roster based on projected points model (this we need to build)

--- modules/Sport.py
+++ modules/Sport.py
@@ -6,6 +6,7 @@ import XMLStats
 import os
 import numpy
 import ast
+from openopt import *
 
 
 #sport_data_dict = {'NHL':{:}}
@@ -89,11 +90,13 @@ class Sport():
 			data_map = {key:player_data_map[key] if 'Stat' in key else key for key in player_data_map} #Cole: updated so only Stat columns change
 			player_key = player_game['Player'] + '_' + player_game['Player_Type']
 			if player_key in player_data_dict.keys():
+				player_data_dict[player_key]['name'].append(player_key) #Cole: adding the field 'name' is required for openopts results output
 				for db_key,player_data in player_game.iteritems():
 					stat_key = data_map[db_key] if db_key in data_map.keys() else db_key
 					player_data_dict[player_key][stat_key].append(player_data)
 			else:
 				player_data_dict[player_key] = {}
+				player_data_dict[player_key]['name'] = [player_key]
 				for db_key,player_data in player_game.iteritems():
 					stat_key = data_map[db_key] if db_key in data_map.keys() else db_key
 					player_data_dict[player_key][stat_key] = [player_data]
@@ -104,7 +107,7 @@ class Sport():
 		for player,stat_dict in stats_data.iteritems():
 			avg_player_stats[player] = {}
 			for stat_key,stat_data in stat_dict.iteritems():
-				try:
+				try: #Cole: nested Try/Except statements feel like they're not best practice...
 					np_array = numpy.array(map(float,stat_data))
 					avg_player_stats[player][stat_key] = numpy.mean(np_array)
 				except TypeError:
@@ -116,11 +119,21 @@ class Sport():
 	def get_FD_player_dict(self,contest_url):
 		return ast.literal_eval(Uds.parse_html(contest_url,"FD.playerpicker.allPlayersFullData = ",";"))
 
+	def optimal_roster(self,forecasted_player_universe):
+		items = ([{key:value for key,value in stats_data.iteritems() if key in self.optimizer_items}
+					 for player_key,stats_data in forecasted_player_universe.iteritems()
+					 if 'Salary' in stats_data.keys()])
+		objective = 'FD_points' #Cole: update this to forecast_points once model is built
+		p = KSP(objective, items, goal = 'max', constraints=self.optimizer_constraints)
+		r = p.solve('glpk',iprint = 0)
+		return r
+
 class MLB(Sport): #Cole: data modelling may need to be refactored, might be more elegant solution
 	def __init__(self):
 		Sport.__init__(self,"MLB")
 		self.FD_data_columns = (['FD_Position','FD_name','MatchupID','TeamID','Dummy2','Salary','PPG',
 									'GamesPlayed','Dummy3','Dummy4','Injury','InjuryAge','Dummy5'])
+		self.positions = ['P','C','1B','2B','3B','SS','OF']
 		self.player_type_map = {'away_batters':'batter','home_batters':'batter','away_pitchers':'pitcher','home_pitchers':'pitcher'}
 		self.db_data_model = collections.OrderedDict({'meta':{'gameid':'GameID','sport':'Sport','type':'Player_Type'}, #Cole: prefix with $ to denote a hard coded value
 							'batter':{'display_name':'Player','position':'Position','team_abbreviation':'Team',
@@ -134,14 +147,24 @@ class MLB(Sport): #Cole: data modelling may need to be refactored, might be more
 		self.inv_db_data_model = {dataset:dict(zip(self.db_data_model[dataset].values(), self.db_data_model[dataset].keys())) for dataset in self.db_data_model}
 		self.data_model = ({'away_batters':self.db_data_model['batter'],'away_pitchers':self.db_data_model['pitcher'],
 							'home_batters':self.db_data_model['batter'],'home_pitchers':self.db_data_model['pitcher']})
+		self.optimizer_constraints = lambda values : (
+    									values['Salary'] <= 55000,
+									    values['P'] == 1,
+									    values['C'] == 1,
+									    values['1B'] == 1,
+									    values['2B'] == 1,
+									    values['3B'] == 1,
+									    values['SS'] == 1,
+									    values['OF'] == 3,)
+		self.optimizer_items = ['name','Player','Player_Type','Salary','P','C','1B','2B','3B','SS','OF','FD_points']
 	
 	def build_player_universe(self,contest_url): #Cole: this desperately needs documentation. Entire data structure needs documentation
-		db_player_data = self.get_db_gamedata()
+		db_player_data = self.avg_stats(self.get_db_gamedata()) #Cole: Stats need to be summarized here (ie avg, max, what ever maths)
 		FD_player_data = self.get_FD_player_dict(contest_url)
 		for FD_playerid,data in FD_player_data.iteritems():
 			FD_data_index = 0
 			FD_name = data[1]
-			if data[0] == 'P':
+			if data[0] == 'P': #Cole: If this can be generalized (ie sport player type map, the entire function can be generalized as a Sport method)
 				player_type = 'pitcher'
 			else:
 				player_type = 'batter'
@@ -149,17 +172,26 @@ class MLB(Sport): #Cole: data modelling may need to be refactored, might be more
 			try:
 				db_player_data[player_key]['FD_playerid'] = FD_playerid
 				for FD_data in data:
-					db_player_data[player_key][self.FD_data_columns[FD_data_index]] = FD_data
+					try:
+						db_player_data[player_key][self.FD_data_columns[FD_data_index]] = float(FD_data)
+					except ValueError:
+						db_player_data[player_key][self.FD_data_columns[FD_data_index]] = FD_data
 					FD_data_index = FD_data_index + 1
-			except KeyError:
+				position_map = {key:1 if key == db_player_data[player_key]['FD_Position'] else 0 for key in self.positions}
+				tmp_dict = position_map.copy()
+				db_player_data[player_key].update(tmp_dict)
+			except KeyError: #Cole: need to map mis-matched names
 				print player_key + ' not in db_player_data'
 		return db_player_data
+
+	def forecast_model(self,points_data):
+		return self.fanduel_points(points_data) #Cole: this will be updated once we have a model, for now just returns the average historical FD points/player
 
 	def fanduel_points(self, points_data):
 		for player,data in points_data.iteritems():
 			if data['Player_Type'] == 'batter':
-				data['point_forecast'] = (data['singles']*1 + data['doubles']*2 + data['triples']*3 + data['home_runs']*4 + data['rbi']*1 + data['runs']*1
+				data['FD_points'] = (data['singles']*1 + data['doubles']*2 + data['triples']*3 + data['home_runs']*4 + data['rbi']*1 + data['runs']*1
 											 + data['walks']*1 + data['stolen_bases']*1 + data['hit_by_pitch'] * 1 - (data['at_bats'] - data['hits'])*.25)
 			else:
-				data['point_forecast'] = data['win']*4 - data['earned_runs']*1 + data['strike_outs']*1 + data['innings_pitched']*1
+				data['FD_points'] = data['win']*4 - data['earned_runs']*1 + data['strike_outs']*1 + data['innings_pitched']*1
 		return points_data
