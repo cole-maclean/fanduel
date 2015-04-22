9acaa7ae509f4427198a61b90e9b9be12c929889
Cole MacLean <maclean.cole@gmail.com>
2015-04-22 10:26:16 -0600

    added Model class, building this class to store model methods. Thought is the the Sport class is at the entire Sport level data (ie all players) and the Model is at single player level. Eventually the Model class will accept a single players data and returns a model for that player

--- modules/Sport.py
+++ modules/Sport.py
@@ -107,39 +107,8 @@ class Sport(): #Cole: Class has functions that should be stripped out and place 
 						player_data_dict[player_key][stat_key] = [player_data]
 		return player_data_dict
 
-	def avg_stat(self,stats_data):
-		np_array = numpy.array(stats_data)
-		avg =numpy.mean(np_array)
-		if numpy.isnan(avg):
-			return 0
-		else:
-			return avg
-
-	def trend_stat(self,stats_data):
-		xi = numpy.arange(0,len(stats_data))
-		matrix_ones = numpy.ones(len(stats_data))
-		array = numpy.array([xi,matrix_ones])
-		return numpy.linalg.lstsq(array.T,stats_data)[0][0]
-
 	def get_FD_player_dict(self,contest_url):
 		return ast.literal_eval(Uds.parse_html(contest_url,"FD.playerpicker.allPlayersFullData = ",";"))
-
-	def get_model_features(self,feature_data,target,spurious_feature):
-		feature_matrix = numpy.array([[feature_data[key][index] for key in feature_data.keys() if key != target] for index in range(0,len(feature_data[target]))])
-		feature_labels = [feature for feature in  feature_data.keys() if feature !=target]
-		spurious_index = feature_labels.index(spurious_feature)
-		target_data = numpy.array(feature_data[target])
-		k = len(feature_labels) - 1
-		feature_scores = SelectKBest(f_classif,k=k).fit(feature_matrix,target_data).pvalues_
-		print feature_scores
-		features = [feature for indx,feature in enumerate(feature_labels) if feature_scores[indx] > feature_scores[spurious_index]]
-		return features
-	# 	for feature in feature_list:
-	# 		sklearn.feature_select #(feature data, traget data, len(feature_list - 1))
-	# 	if spurious_feature in pruned_feature_list:
-	# 		return get_model_features(...)
-	# 	else:
-	# 		return pruned_feature_list
 
 	def optimal_roster(self,forecasted_player_universe):
 		items = ([{key:value for key,value in stats_data.iteritems() if key in self.optimizer_items}
@@ -220,28 +189,6 @@ class MLB(Sport): #Cole: data modelling may need to be refactored, might be more
 			FD_points= (numpy.array(data['win'])*4+numpy.array(data['earned_runs'])*-1+
 								numpy.array(data['strike_outs'])*1+numpy.array(data['innings_pitched'])*1)
 		return FD_points
-
-	def build_feature_dataset(self,hist_data):#Cole: How do we generalize this method. Some out-of-box method likely exists. Defs need to refactor
-		feature_data = {}
-		for player,stats_data in hist_data.iteritems():
-			FD_points = self.FD_points(stats_data)
-			feature_dict = {}
-			feature_dict['FD_points'] = []
-			feature_dict['five_day_avg'] = []
-			feature_dict['five_day_trend'] = []
-			feature_dict['day'] = []
-			for indx,FD_point in enumerate(FD_points):
-				reverse_index = len(FD_points)-indx
-				try:
-					chunk_list = [FD_points[chunk_indx] for chunk_indx in range(reverse_index-5,reverse_index-1)]
-					feature_dict['FD_points'].append(FD_point)
-					feature_dict['five_day_avg'].append(self.avg_stat(chunk_list))
-					feature_dict['five_day_trend'].append(self.trend_stat(chunk_list))
-					feature_dict['day'].append(int(str(stats_data['Date'][indx])[6:8]))
-				except IndexError:
-					break
-			feature_data[player] = feature_dict
-		return feature_data
 
 
 
